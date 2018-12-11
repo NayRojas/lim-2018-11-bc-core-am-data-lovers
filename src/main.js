922304b66fbf9a02a3f78af4318697f1391c35e5
Nay <nayllen.rojas@gmail.com>
2018-12-11 15:39:34 -0500

    Primera estructura de HTML - Pantalla 1- Historia de usuario 1

---
+++ src/main.js
@@ -0,0 +1,5 @@
+//escoger la data 5 campeones
+//Mostrar Nombre del champion, titulo, Imagen principal, Descripción
+//Mostrar su nivel de ataque, defense, resistencia, nivel de dificultad
+//Mostrar que tipo de rol juega
+
