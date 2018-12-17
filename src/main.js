9bb28d6aa402d3def347b39b1b2b4fd2ff2659ad
nayrojas <nayllen.rojas@gmail.com>
2018-12-17 06:54:02 -0500

    modified:   css/style.css 	modified:   index.html 	copied:     index.html -> index2.html 	modified:   main.js

--- src/main.js
+++ src/main.js
@@ -51,14 +51,148 @@ const gettingFreeChampeons3 = entriesOfChampeons.map(currentChampeons => allCham
 //console.log(gettingFreeChampeons3)
 for (var [key, value] of entriesOfChampeons) {
     if (value.top ==='top5') {
-        console.log("Campion de la semana:" + value.name)
+        //console.log(value)
     }
 }
+var newArray = entriesOfChampeons.map(currentChampeon => currentChampeon[1]).filter(currentChampeon => currentChampeon.top ==='top5')
 
-//var newArray = entriesOfChampeons.filter(function(currentChampeon){
-//    return currentChampeon.top === 'top5'
-//});
-//console.log(newArray)
+     
+//******************JS TEMPLATE FOR TOP 5****************************
+
+
+//ESTE ES EL TEMPLATE
+
+   
+
+
+
+window.onload = function(){
+
+    for (i = 0; i <  newArray.length; i++) { 
+
+                //Aqui he creado el div(Clase y el ID) madre del primer campeon
+        const fatherDiv = document.createElement("div");
+        const valueFatherClass = document.createAttribute("class");
+        valueFatherClass.value = "campeones elemento primer-campeon";
+        fatherDiv.setAttributeNode(valueFatherClass);
+
+        const valueFatherId = document.createAttribute("id");
+        valueFatherId.value = "primer-campeon";
+        fatherDiv.setAttributeNode(valueFatherId);
+        document.getElementById("descripion-de-los-top-5").appendChild(fatherDiv);
+
+        //Aqui he creado el img principal del primer campeon
+            const champeonImage = document.createElement("img");
+            const champeonImageClass = document.createAttribute("class");
+            champeonImageClass.value = "img-del-campeon";
+            champeonImage.setAttributeNode(champeonImageClass);
+        //su alt
+            const champeonImageAlt = document.createAttribute("alt");
+            champeonImageAlt.value = "imagen-del-campeon";
+            champeonImage.setAttributeNode(champeonImageAlt);
+        //su src
+            const champeonImageSrc = document.createAttribute("src");
+            champeonImageSrc.value = newArray[i].splash;
+            champeonImage.setAttributeNode(champeonImageSrc);
+            document.getElementById("primer-campeon").appendChild(champeonImage);
+
+        //Aqui he creado el nombre del primer campeon
+        //su clase
+        const champeonName = document.createElement("h4");
+        const champeonNameClass = document.createAttribute("class");
+        champeonNameClass.value = "nombre-del-campeon";
+        champeonName.setAttributeNode(champeonNameClass);
+
+        //su nombre en text
+        const champeonNameText = document.createTextNode(newArray[i].name);
+        champeonName.appendChild(champeonNameText);
+        document.getElementById("primer-campeon").appendChild(champeonName);
+
+        //Aqui he creado el titulo del primer campeon
+        //su clase
+        const champeonTitle = document.createElement("p");
+        const champeonTitleClass = document.createAttribute("class");
+        champeonTitleClass.value = "titulo-del-campeon";
+        champeonTitle.setAttributeNode(champeonTitleClass);
+
+        //su titulo en text
+        const champeonTitleName = document.createTextNode(newArray[i].title);
+        champeonTitle.appendChild(champeonTitleName);
+        document.getElementById("primer-campeon").appendChild(champeonTitle);
+
+        //Aqui he creado el blurb del primer campeon
+        //su clase
+        const champeonBlurb = document.createElement("p");
+        const champeonBlurbClass = document.createAttribute("class");
+        champeonBlurbClass.value = "blurb-del-campeon";
+        champeonBlurb.setAttributeNode(champeonBlurbClass);
+
+        //su titulo en text
+        const champeonBlurbText = document.createTextNode(newArray[i].blurb);
+        champeonBlurb.appendChild(champeonBlurbText);
+        document.getElementById("primer-campeon").appendChild(champeonBlurb);
+
+        const divLevels = document.createElement("div");
+
+        const divLevelsId = document.createAttribute("id");
+        divLevelsId.value = "niveles-del-campeon";
+        divLevels.setAttributeNode(divLevelsId);
+
+        const divLevelsClass = document.createAttribute("class");
+        divLevelsClass.value = "niveles-del-campeon";
+        divLevels.setAttributeNode(divLevelsClass);
+
+        document.getElementById("primer-campeon").appendChild(divLevels);
+
+        // Info
+        info = Object.entries(newArray[i].info)
+
+        
+        for(index = 0; index <  3; index++) {
+            const champeonLevels = document.createElement("LABEL");
+
+            keys = Object.keys(info[index])
+            values = Object.values(info[index])
+
+            console.log("current key: " + keys[index])
+            console.log("current value: " + values[index])
+            
+            if (keys[steps]=='attack') {
+                const champeonLevelsText = document.createTextNode(keys[steps])
+                champeonLevels.appendChild(champeonLevelsText)
+
+                const champeonLevelsClass = document.createAttribute("class");
+                champeonLevelsClass.value = "icon-gitlab";
+                champeonLevels.setAttributeNode(champeonLevelsClass);
+                
+                const champeonLevelsFor = document.createAttribute("for");
+                champeonLevelsFor.value = keys[steps]
+                champeonLevels.setAttributeNode(champeonLevelsFor)
+
+                const champeonLevelsSpan = document.createElement("SPAN");
+                const champeonLevelsSpanText = document.createTextNode(values[steps])
+                champeonLevelsSpan.appendChild(champeonLevelsSpanText)
+                
+                champeonLevels.appendChild(champeonLevelsSpan)
+
+                document.getElementById("niveles-del-campeon").appendChild(champeonLevels);
+            }
+        }
+   
+    }
+
+ }
+      
+
+      
+
+      
+      
+
+      
+      
+ 
+    
  
 //Arroja el campeon y RECORRE LOS INDICES
 //escoger la data 5 campeones
